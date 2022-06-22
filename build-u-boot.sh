6436947fe944d6f1248068ccaf244e26ead85700
XYenChi <oriachiuan@gmail.com>
2022-06-22 11:32:43 +0800

    download patch 005 and changed build-u-boot.sh

--- build-u-boot.sh
+++ build-u-boot.sh
@@ -3,6 +3,7 @@ cd $PWD/repos/u-boot/
 git clean -fdx
 git reset --hard origin/master
 git apply $CWD/patches/u-boot/riscv-Fix-build-against-binutils-2.38.diff
+git apply $CWD/patches/u-boot/0005-board-sifive-spl-Set-remote-thermal-of-TMP451-to-85-.patch
 export OPENSBI=$CWD/install/share/opensbi/lp64/generic/firmware/fw_dynamic.bin
 make sifive_unmatched_defconfig
 make -j16
