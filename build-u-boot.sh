90117fec199b5dd974d25c8c2b7bff0e021a2eb3
XYenChi <oriachiuan@gmail.com>
2022-06-22 18:00:50 +0800

    applied 2/3/4/5/7 patches

--- build-u-boot.sh
+++ build-u-boot.sh
@@ -3,7 +3,11 @@ cd $PWD/repos/u-boot/
 git clean -fdx
 git reset --hard origin/master
 git apply $CWD/patches/u-boot/riscv-Fix-build-against-binutils-2.38.diff
+git apply $CWD/patches/u-boot/0002-board-sifive-spl-Initialized-the-PWM-setting-in-the-.patch
+git apply $CWD/patches/u-boot/0003-board-sifive-Set-LED-s-color-to-purple-in-the-U-boot.patch
+git apply $CWD/patches/u-boot/0004-board-sifive-Set-LED-s-color-to-blue-before-jumping-.patch
 git apply $CWD/patches/u-boot/0005-board-sifive-spl-Set-remote-thermal-of-TMP451-to-85-.patch
+git apply $CWD/patches/u-boot/0007-riscv-sifive-unmatched-disable-FDT-and-initrd-reloca.patch
 export OPENSBI=$CWD/install/share/opensbi/lp64/generic/firmware/fw_dynamic.bin
 make sifive_unmatched_defconfig
 make -j16
