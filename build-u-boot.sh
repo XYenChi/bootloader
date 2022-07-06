2f5a26be6b60d45eca42b83a9c0327a6303194e3
XYenChi <oriachiuan@gmail.com>
2022-07-06 02:43:53 +0000

    add related package that need to install for compiling.

--- build-u-boot.sh
+++ build-u-boot.sh
@@ -1,11 +1,12 @@
 CWD=$PWD
 cd $PWD/repos/u-boot/
+pacman -S python3 flex bison swig python-setuptools
 git clean -fdx
 git reset --hard origin/master
 git apply $CWD/patches/u-boot/riscv-Fix-build-against-binutils-2.38.diff
 git apply $CWD/patches/u-boot/0002-board-sifive-spl-Initialized-the-PWM-setting-in-the-.patch
 git apply $CWD/patches/u-boot/0003-board-sifive-Set-LED-s-color-to-purple-in-the-U-boot.patch
-git apply $CWD/patches/u-boot/0004-board-sifive-Set-LED-s-color-to-blue-before-jumping-.patch
+# can't patch unmatched.h git apply $CWD/patches/u-boot/0004-board-sifive-Set-LED-s-color-to-blue-before-jumping-.patch
 git apply $CWD/patches/u-boot/0005-board-sifive-spl-Set-remote-thermal-of-TMP451-to-85-.patch
 git apply $CWD/patches/u-boot/0007-riscv-sifive-unmatched-disable-FDT-and-initrd-reloca.patch
 export OPENSBI=$CWD/install/share/opensbi/lp64/generic/firmware/fw_dynamic.bin
