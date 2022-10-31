f1ae9a1049206ebe78ec49fa5c8b618131602d03
Avimitin <avimitin@gmail.com>
2022-10-31 15:11:24 +0800

    Download riscv pacman config from upstream
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- install-deps.sh
+++ install-deps.sh
@@ -8,6 +8,9 @@ if [[ "Arch Linux" != "$NAME" ]]; then
 fi
 
 pacman -Syu --needed --noconfirm \
-  git base-devel \
+  git base-devel curl\
   python3 flex bison swig python-setuptools \
   gptfdisk arch-install-scripts riscv64-linux-gnu-gcc
+
+curl -fLo pacman-extra-riscv64.conf \
+  "https://raw.githubusercontent.com/archlinuxcn/repo/master/archlinuxcn/devtools-riscv64/pacman-extra-riscv64.conf"
