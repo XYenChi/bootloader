fc9bb799ca0724f4228c6e1e9196258bb58ece9d
Avimitin <avimitin@gmail.com>
2022-10-31 17:57:24 +0800

    Add qemu-user-binfmt
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- install-deps.sh
+++ install-deps.sh
@@ -10,7 +10,7 @@ fi
 pacman -Syu --needed --noconfirm \
   git base-devel curl\
   python3 flex bison swig python-setuptools \
-  gptfdisk arch-install-scripts riscv64-linux-gnu-gcc qemu-user-static qemu-user-static-binfmt
+  gptfdisk arch-install-scripts riscv64-linux-gnu-gcc qemu-user-binfmt qemu-user-static qemu-user-static-binfmt
 
 curl -fLo pacman-extra-riscv64.conf \
   "https://raw.githubusercontent.com/archlinuxcn/repo/master/archlinuxcn/devtools-riscv64/pacman-extra-riscv64.conf"
